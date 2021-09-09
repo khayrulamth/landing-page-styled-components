1af77717256c5bd149e077967f9a9a557b9d15d2
Md. Khayrul Islam <devkhayrul@gmail.com>
2021-09-10 00:51:17 +0600

    hero section done

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {HeroContainer,HeroBg,VideoBg,HeroContent, HeroH1,HeroP, HeroBtnWrapper,ArrowForward, ArrowRight} from './HeroElements';
+import Video from '../../videos/video.mp4';
+import {Button} from '../../components/ButtonElements';
 
 const Hero = () => {
+    const [hover,setHover] = useState(false);
+    const onHover = () =>{
+        setHover(!hover);
+    }
     return (
-        <div>
-            
-        </div>
+        <HeroContainer id ="home">
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>Sign Up for a new account and receive a $250 in credit towards your next payment. </HeroP>
+                <HeroBtnWrapper>
+                    <Button 
+                    onMouseEnter={onHover} 
+                    onMouseLeave={onHover} 
+                    primary = "true"
+                    dark = "true"
+                    to="signup"> Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
     );
 };
 
